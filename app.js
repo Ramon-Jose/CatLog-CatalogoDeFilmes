965ba51c6046ac3a7b40362d916df8a31bbd8299
Ramon José <ramonassis_bh@hotmail.com>
2025-04-20 21:51:44 -0300

    Envio dos arquivos index.html, styles.css, app.js e img

---
+++ app.js
@@ -0,0 +1,25 @@
+const filmePrincipal = [
+    {"id": "1", "filme": "The Electric State", "ano": "2025", "diretor": "Anthony Russo e Joe Russo", "descricao": "Uma adolescente órfã pega a estrada com um robô misterioso em busca do irmão há muito tempo desaparecido, encontrando no caminho um contrabandista e seu ajudante hilário.", "imagem": "./img/eletricState.png"},
+
+    {"id": "2", "filme": "Oppenheimer", "ano": "2023", "diretor": "Christopher Nolan", "descricao": "O físico J. Robert Oppenheimer trabalha com uma equipe de cientistas durante o Projeto Manhattan, levando ao desenvolvimento da bomba atômica.", "imagem": "./img/oppenheimer.png"},
+
+    {"id": "3", "filme": "A Hospedeira", "ano": "2013", "diretor": "Andrew Niccol", "descricao": "A Terra foi colonizada por uma raça alienígena que controla a mente dos humanos e usa seus corpos como hospedeiros. A maioria da humanidade foi erradicada, mas quando Melanie se recusa a aceitar sua nova alma, uma caçada se inicia.", "imagem": "./img/aHospedeira.png"}
+];
+
+const filmes = [
+    {"id": "4", "filme": "Conclave", "ano": "2024", "diretor": "Edward Berger", "descricao": "Com a morte do Papa, o cardeal Lawrence reúne um grupo de sacerdotes para eleger seu sucessor. Cercado por líderes do mundo todo nos corredores do Vaticano, ele descobre uma trilha de segredos profundos que podem abalar a própria fundação da Igreja.", "imagem": "./img/conclave.png"},
+
+    {"id": "5", "filme": "O Senhor dos Anéis: O Retorno do Rei", "ano": "2004", "diretor": "Peter Jackson", "descricao": "Sauron planeja um grande ataque a Minas Tirith, capital de Gondor, o que faz com que Gandalf e Pippin partam para o local na intenção de ajudar a resistência. Frodo, Sam e Gollum seguem sua viagem rumo à Montanha da Perdição para destruir o anel.", "imagem": "./img/senhorDosAneis.png"},
+
+    {"id": "6", "filme": "Como Treinar o Seu Dragão 2", "ano": "2014", "diretor": "Dean DeBlois", "descricao": "Cinco anos se passaram desde que Soluço estabeleceu a paz com os dragões e vive em harmonia, na Ilha de Berk, com Banguela. Eles voam, apostam corridas e se divertem muito. Em uma destas aventuras, descobrem uma caverna secreta cheia de dragões. Agora, a dupla luta para proteger Berk de um guerreiro perigoso, chamado Drago Bludvist, que deseja controlar todos os dragões existentes.", "imagem": "./img/comoTreinaroSeuDragao.png"}
+];
+
+const filmesAside = [
+{"id": "7", "filme": "Mickey 17", "ano": "2025", "diretor": "Bong Joon-ho", "descricao": "Um colaborador é enviado em uma expedição humana para colonizar o mundo gelado de Niflheim. Após uma iteração morrer, um novo corpo é regenerado com a maioria de suas memórias intactas.", "imagem": "./img/mickey17.png"},
+
+{"id": "8", "filme": "Anora", "ano": "2024", "diretor": "Sean S. Baker", "descricao": "Anora, uma jovem stripper do Brooklyn, conhece o filho de um oligarca russo na boate em que trabalha. Os dois engatam um improvável romance e Anora vive uma história de Cinderela contemporânea por alguns dias. Em Las Vegas, o casal resolve consumar o relacionamento intempestivo e se casa de forma impulsiva.", "imagem": "./img/anora.png"},
+
+{"id": "9", "filme": "Ainda Estou Aqui", "ano": "2024", "diretor": "Walter Salles", "descricao": "No início da década de 1970, o Brasil enfrenta o endurecimento da ditadura militar. No Rio de Janeiro, a família Paiva - Rubens, Eunice e seus cinco filhos - vive à beira da praia em uma casa de portas abertas para os amigos. Um dia, Rubens Paiva é levado por militares à paisana e desaparece.", "imagem": "./img/AindaEstouAqui.png"},
+
+{"id": "10", "filme": "Midsommar - O Mal Não Espera a Noite", "ano": "2019", "diretor": "Ari Asters", "descricao": "Após vivenciar uma tragédia pessoal, Dani vai com o namorado Christian e um grupo de amigos até a Suécia para participar de um festival local de verão. Mas, ao invés das férias tranquilas com a qual todos sonhavam, o grupo se depara com rituais bizarros de uma adoração pagã.", "imagem": "./img/midsommar.png"}
+];
